934f25b9b09cd6ae38d15b6f01ec7d17353f87f7
Dingyuan Wang <abcdoyle888@gmail.com>
2015-07-26 22:47:10 +0800

    save the context aware version of say.py

--- say.py
+++ say.py
@@ -4,7 +4,10 @@
 import re
 import sys
 import kenlm
+import jieba
+import pickle
 import random
+import itertools
 
 RE_UCJK = re.compile(
     '([\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\U00020000-\U0002A6D6]+)')
@@ -27,8 +30,6 @@ def loaddict(fn):
             dic.add(w)
     return list(dic)
 
-
-voc = loaddict(sys.argv[2])
 
 #voc = '，；。！？' + ''.join(chr(x) for x in range(0x4e00, 0x9fa5))
 
@@ -56,17 +57,17 @@ def sel_best(weights):
     return max(enumerate(weights), key=lambda x: x[1])
 
 
-def generate_word(lm, order):
+def generate_word(lm, order, ctxvoc):
     out = []
-    idx, w = weighted_choice_king(10**lm.score(c, 1, 0) for c in voc)
-    sys.stdout.buffer.write(voc[idx].encode('utf-8'))
-    out.append(voc[idx])
+    idx, w = weighted_choice_king(10**lm.score(c, 1, 0) for c in ctxvoc)
+    sys.stdout.buffer.write(ctxvoc[idx].encode('utf-8'))
+    out.append(ctxvoc[idx])
     while 1:
         bos = (len(out) <= order + 2)
         history = ' '.join(out[-order - 2:]) + ' '
         idx, w = weighted_choice_king(
-            10**lm.score(history + voc[k // 2], bos, k % 2) for k in range(len(voc) * 2))
-        c = voc[idx // 2]
+            10**lm.score(history + ctxvoc[k // 2], bos, k % 2) for k in range(len(ctxvoc) * 2))
+        c = ctxvoc[idx // 2]
         cprint(c)
         out.append(c)
         if idx % 2 or (len(out) > 3 and all(i == out[-1] for i in out[-3:])):
@@ -75,6 +76,9 @@ def generate_word(lm, order):
 
 LM = kenlm.LanguageModel(sys.argv[1])
 order = LM.order
+voc = loaddict(sys.argv[2])
+ctx = pickle.load(open(sys.argv[3], 'rb'))
+
 for ln in sys.stdin:
-    num = int(ln.strip())
-    [generate_word(LM, order) for i in range(num)]
+    ctxvoc = list(frozenset(voc).intersection(frozenset(itertools.chain.from_iterable(ctx.get(w, ()) for w in frozenset(jieba.cut(ln.strip(), HMM=False))))))
+    generate_word(LM, order, ctxvoc)
